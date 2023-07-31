82d59aa769516a7eff1df1a52a45a811ad86a51d
choijihye295 <choijihye295@gmail.com>
2023-07-31 19:53:04 +0900

    h5

--- myweb/settings.py
+++ myweb/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'myweb.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -98,6 +98,10 @@ DATABASES = {
 #추가
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+'''
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=500)
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
