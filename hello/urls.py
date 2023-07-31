016566f7e3414b2c446efecedc39be55d9b1079b
choijihye295 <choijihye295@gmail.com>
2023-07-31 20:31:40 +0900

    h8

--- hello/urls.py
+++ hello/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
 from .views import HelloAPI
 
+'''
 urlpatterns = [
     path("hello/", HelloAPI),
+]
+'''
+
+urlpatterns = [
+    path("hello/", HelloAPI, name="hello-api"),
 ]
