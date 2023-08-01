25ba2411077e801b14bc8b868c4e3f2684e898c4
choijihye295 <choijihye295@gmail.com>
2023-08-01 16:41:01 +0900

    h14

--- myweb/settings.py
+++ myweb/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'myweb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), #수정
+        'NAME': BASE_DIR / 'db.sqlite3', #수정
 
     }
 }
